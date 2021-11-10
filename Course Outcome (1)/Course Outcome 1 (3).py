deaaab8ace16b2c0704c417071f8caa1c1aca713
Gayathri Chandralal <gaya3thannickal@gmail.com>
2021-11-10 21:24:45 +0530

    python work

---
+++ Course Outcome (1)/Course Outcome 1 (3).py
@@ -0,0 +1,17 @@
+
+num1=[-4,-3,-2,-1,1,2,3,4,-5,-6,-7,8,9]
+num2=[x for x in num1 if x>0]
+print(num2)
+
+n=int(input("Enter the limit="))
+print([x*x for x in range(n+1)])
+
+word=input("Enter a word=")
+vowel=["A","E","I","O","U","a","e","i","o","u"]
+listv=[x for x in word if x in vowel ]
+print(listv)
+
+word=input("Enter a word=")
+listo=[ord(x) for x in word]
+print(listo)
+
---
+++ Course Outcome (1)/Course Outcome 1 (4).py
@@ -0,0 +1,3 @@
+text1=input("Enter a line of text=")
+text2=text1.split(" ")
+print("number of words in the line of text=",len(text2))
